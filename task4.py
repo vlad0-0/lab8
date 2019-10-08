77c065152600cbde29ba15fd91e30f08931994e8
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-10-08 15:36:19 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -3,3 +3,11 @@ y = float(input())
 while x == 0 or y == 0:
   x = float(input())
   y = float(input())
+if x > 0 and y > 0:
+  print("1")
+elif x < 0 and y > 0:
+  print("2")
+elif x < 0 and y < 0:
+  print("3")
+elif x > 0 and y < 0:
+  print("4")
